d7d51bdcf13fc20eff3b57c84bcbe4c360575315
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:18:45 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "inspector" {
+  description = "The AWS Inspector module outputs"
+  value       = module.inspector
 }
